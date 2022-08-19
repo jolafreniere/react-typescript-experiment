be53b4af5d1d312433c22760d6a79a5615a0e623
Jonathan Lafrenière <46546590+jolafreniere@users.noreply.github.com>
2022-08-19 04:25:42 -0400

    useState hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import UserList from './UserList';
 function App() {
-  const users = [{"name": "John Doe", "age": 30}, {"name": "Jane Doe", "age": 25}];
+  const [users, setUsers] = useState([{"name": "John Doe", "age": 30}, {"name": "Jane Doe", "age": 25}]);
   return (
     <div className="App">
       <h1>Users</h1>
